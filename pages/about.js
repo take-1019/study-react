998624f750ce67200a4f10efcfda0dd7b8acca88
take-1019 <tkchtmy@gmail.com>
2022-08-01 23:44:24 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Footer } from '../components/Footer';
+import Headline from '../components/Headline';
 import Links from '../components/Links';
 import styles from '../styles/Home.module.css';
 
@@ -14,14 +15,8 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					about page <a href="https://nextjs.org">Next.js!</a>
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing <code className={styles.code}>about/index.js</code>
-				</p>
-<Links />
+				<Headline page="about" />
+				<Links />
 			</main>
 			<Footer />
 		</div>
