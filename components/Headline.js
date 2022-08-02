ccd3373b43c21726a8d34ba9170d8e2c0b10b3c4
take-1019 <tkchtmy@gmail.com>
2022-08-02 20:09:38 +0900

    💄 スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,10 +1,10 @@
-import styles from '../styles/Home.module.css';
+import classes from './Headline.module.css';
 
 export function Headline(props) {
 	return (
 		<div>
-			<h1 className={styles.title}>{props.page} Page</h1>
-			<p className={styles.description}>Get started by editing {props.children}</p>
+			<h1 className={classes.title}>{props.page} Page</h1>
+			<p className={classes.description}>Get started by editing {props.children}</p>
 		</div>
 	);
 }
