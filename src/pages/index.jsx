290104010c62a4a50ae17ec16edeed37d262c9fa
take-1019 <tkchtmy@gmail.com>
2022-08-05 08:12:07 +0900

    ♻️ インポートを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css';
 import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
 	const foo = 1;
