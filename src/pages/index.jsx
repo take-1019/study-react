a2bcea7a4b60c1f9dd0522357fe53a89309d9627
take-1019 <tkchtmy@gmail.com>
2022-08-05 08:27:10 +0900

    ✨ 講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { useCallback } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
 	const foo = 1;
@@ -12,6 +13,13 @@ export default function Home() {
 		console.log(e.target.href);
 		e.preventDefault();
 		alert(foo);
+	}, []);
+
+	useEffect(() => {
+		document.body.style.backgroundColor = 'lightblue';
+		return () => {
+			document.body.style.backgroundColor = '';
+		};
 	}, []);
 
 	return (
