2d4e2d41440eb183dc0a4db3c8336e6ebc875237
take-1019 <tkchtmy@gmail.com>
2022-08-04 23:26:07 +0900

    ⚗️ 試しにクリックイベントを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,23 @@ import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
 export default function Home() {
+	const foo = 1;
+
+	const handleClick = useCallback((e) => {
+		console.log(e.target.href);
+		e.preventDefault();
+		alert(foo);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index page</title>
 			</Head>
 			<Header />
+			<a href="/about" onClick={handleClick}>
+				ボタン
+			</a>
 			<Main page="index" />
 			<Footer />
 		</div>
