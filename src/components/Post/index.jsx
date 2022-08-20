55254f0298dd3eb6e280867e3228a5414fc9ecdf
take-1019 <tkchtmy@gmail.com>
2022-08-20 15:47:36 +0900

    ✨ ユーザーのfetch,fetcherの共通化

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,27 @@
+import { usePost } from 'src/hooks/usePost';
+import Head from 'next/head';
+
+export const Post = () => {
+	const { post, user, error, isLoading } = usePost();
+
+	if (isLoading) {
+		return <div>ローディング中</div>;
+	}
+
+	if (error) {
+		return <div>{error.message}</div>;
+	}
+
+	return (
+		<div>
+			<Head>
+				<title>{post?.title}</title>
+			</Head>
+			<h1>{post?.title}</h1>
+			<p>{post?.body}</p>
+			{user?.name ? <div>Creted By {user.name}</div> : null}
+		</div>
+	);
+};
+
+export default Post;
