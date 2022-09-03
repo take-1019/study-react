a4b68754841f96ffa3f37f0deb84fc27bad23a7b
take-1019 <tkchtmy@gmail.com>
2022-09-03 11:44:35 +0900

    ⚡️ コメント一覧のページをSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,15 +1,33 @@
 import Head from 'next/head';
 import { Header } from 'src/components/Header';
 import { Commetns as Commentscomponent } from 'src/components/comments';
+import { SWRConfig } from 'swr';
 
-const Comments = () => {
+export const getStaticProps = async () => {
+	const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+	const comments = await fetch(COMMENTS_API_URL);
+	const commentsData = await comments.json();
+
+	return {
+		props: {
+			fallback: {
+				[COMMENTS_API_URL]: commentsData,
+			},
+		},
+	};
+};
+
+const Comments = (props) => {
+	const { fallback } = props;
 	return (
 		<div>
 			<Head>
 				<title>Comments page</title>
 			</Head>
-			<Header />
-			<Commentscomponent />
+			<SWRConfig value={{ fallback }}>
+				<Header />
+				<Commentscomponent />
+			</SWRConfig>
 		</div>
 	);
 };
